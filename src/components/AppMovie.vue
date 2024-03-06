69551de4d66584e128ec6c3dc8021d8044a959b4
giuseppe_cassone <giuseppecassone7@gmail.com>
2024-03-06 22:52:58 +0100

    milestone 3 completata

--- src/components/AppMovie.vue
+++ src/components/AppMovie.vue
@@ -13,8 +13,27 @@
             },
 
             posterImg() {
-                return `https://image.tmdb.org/t/p/w342/${this.movies.poster_path}.jpg`;
-            }
+                return `https://image.tmdb.org/t/p/w300/${this.movies.poster_path}.jpg`;
+            },
+
+            convertVotes() {
+                return Math.round(this.movies.vote_average / 2);
+            },
+
+            generateStars() {
+                const numberOfStars = this.convertVotes();
+                const stars = [];
+
+                for (let i = 0; i < numberOfStars; i++) {
+                    stars.push('fa-solid fa-star');
+                }
+
+                for (let i = numberOfStars; i < 5; i++) {
+                    stars.push('fa-regular fa-star');
+                }
+
+                return stars;
+            },
         }
     }
 
@@ -27,7 +46,7 @@
         <h2>{{ movies.title }}</h2>
         <h3>{{ movies.original_title }}</h3>
         <div><img :src="flagLanguage(movies.original_language)" alt=""></div>
-        <span>{{ movies.vote_average }}</span>
+        <div class="d-flex justify-content-center"><i v-for="star in generateStars()" :class="star"></i></div>
     </div>
 
 </template>
--- src/components/AppSeries.vue
+++ src/components/AppSeries.vue
@@ -13,8 +13,27 @@
             },
 
             posterImg() {
-                return `https://image.tmdb.org/t/p/w342/${this.series.poster_path}.jpg`;
-            }
+                return `https://image.tmdb.org/t/p/w300/${this.series.poster_path}.jpg`;
+            },
+
+            convertVotes() {
+                return Math.round(this.series.vote_average / 2);
+            },
+
+            generateStars() {
+                const numberOfStars = this.convertVotes();
+                const stars = [];
+
+                for (let i = 0; i < numberOfStars; i++) {
+                    stars.push('fa-solid fa-star');
+                }
+
+                for (let i = numberOfStars; i < 5; i++) {
+                    stars.push('fa-regular fa-star');
+                }
+
+                return stars;
+            },
         }
     }
 
@@ -22,12 +41,12 @@
 
 <template>
 
-    <div class="card gap-2 text-center">
+    <div class="card text-center">
         <div><img :src="posterImg()" alt=""></div>
         <h2>{{ series.name }}</h2>
         <h3>{{ series.original_name }}</h3>
         <div><img :src="flagLanguage(series.original_language)" alt=""></div>
-        <span>{{ series.vote_average }}</span>
+        <div class="d-flex justify-content-center"><i v-for="star in generateStars()" :class="star"></i></div>
     </div>
 
 </template>
