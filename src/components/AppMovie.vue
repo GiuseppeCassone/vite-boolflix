900b2b4f3d89b8c561f5413610c5b8f96ffa3e97
giuseppe_cassone <giuseppecassone7@gmail.com>
2024-03-06 19:27:20 +0100

    inserito le bandiere al posto della lingua

--- src/components/AppMovie.vue
+++ src/components/AppMovie.vue
@@ -6,16 +6,22 @@
         props: {
             movies: Object,
         },
+
+        methods: {
+            flagLanguage(flag) {
+                return `https://flagcdn.com/16x12/${flag}.png`;
+            }
+        }
     }
 
 </script>
 
 <template>
 
-    <div class="card gap-2">
+    <div class="card gap-2 text-center">
         <h2>{{ movies.title }}</h2>
         <h3>{{ movies.original_title }}</h3>
-        <span>{{ movies.original_language }}</span>
+        <div><img :src="flagLanguage(movies.original_language)" alt=""></div>
         <span>{{ movies.vote_average }}</span>
     </div>
 
