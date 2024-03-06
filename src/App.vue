991620de77125b3d8c6d03332508ca8a75fa6a73
giuseppe_cassone <giuseppecassone7@gmail.com>
2024-03-06 12:55:10 +0100

    milestone 0 completata

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <script>
+  import axios from 'axios';
  import { store } from './store.js';
+
+ import AppNav from './components/AppNav.vue';
+ import AppMain from './components/AppMain.vue';
 
  export default {
 
@@ -10,12 +14,24 @@
         }
       },
 
+      created() {
+        axios.get('https://api.themoviedb.org/3/search/movie?api_key=39cad4f2c9bf7824ade8563d23362a6f&query=ritorno+al+futuro').then(res => {
+          this.store.filmList = res.data.results;
+          console.log(res.data.results);
+        })
+      },
+
+      components: {
+        AppNav,
+        AppMain,
+      }
+
 }
 
 </script>
 
 <template>
-
+  <AppNav></AppNav>
 </template>
 
 <style lang="scss">
