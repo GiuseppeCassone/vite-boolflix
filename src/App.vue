3d3725576ef90f7e9e3572d1f5754363c98bc3c1
giuseppe_cassone <giuseppecassone7@gmail.com>
2024-03-08 12:35:47 +0100

    controllo del non risultato aggiunto all'AppMain

--- src/App.vue
+++ src/App.vue
@@ -37,13 +37,6 @@
             axios.get('https://api.themoviedb.org/3/search/tv?api_key=39cad4f2c9bf7824ade8563d23362a6f&query=' + this.store.searchText).then(res => {
               this.store.tvSeriesList = res.data.results; 
               console.log(res.data.results)
-
-              this.store.tvSeriesList.forEach(series => {
-                axios.get(`https://api.themoviedb.org/3/movie/${series.id}/credits?api_key=39cad4f2c9bf7824ade8563d23362a6f`)
-                  .then(res => {
-                    this.store.filmCastList[series.id] = res.data.cast;
-                  })  
-                });
               
             });
           }
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -27,12 +27,18 @@
     <div class="container pt-5">
         <div class="d-flex flex-wrap mb-5">
             <h1 class="text-white w-100">Movies</h1>
-            <AppCard v-for="currentMovie in store.filmList" :infos="currentMovie" :cast="store.filmCastList[currentMovie.id]"></AppCard>
+            <div class="cards-container d-flex flex-wrap w-100" v-if="store.filmList.length > 0">
+                <AppCard v-for="currentMovie in store.filmList" :infos="currentMovie" :cast="store.filmCastList[currentMovie.id]"></AppCard>
+            </div>
+            <div class="no-results text-white" v-else>NESSUN FILM TROVATO CON QUESTA RICERCA</div>
         </div>
 
         <div class="d-flex flex-wrap">
             <h1 class="text-white w-100 ">Series</h1>
-            <AppCard v-for="currentSeries in store.tvSeriesList" :infos="currentSeries" :cast="store.tvSeriesCastList[currentSeries.id]"></AppCard>           
+            <div class="cards-container d-flex flex-wrap w-100" v-if="store.tvSeriesList.length > 0">
+                <AppCard v-for="currentSeries in store.tvSeriesList" :infos="currentSeries" :cast="store.tvSeriesCastList[currentSeries.id]"></AppCard>
+            </div>
+            <div class="no-results text-white" v-else>NESSUNA SERIE TV TROVATA CON QUESTA RICERCA</div>           
         </div>       
     </div>
 
